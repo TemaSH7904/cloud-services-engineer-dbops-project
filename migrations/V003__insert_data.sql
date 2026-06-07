2cf1b91e7eea6e62270d5476073450e6dfdd7ffc
TemaSH7904 <artem_klim1@mail.ru>
2026-06-07 15:47:31 +0000

    Reduce V003 data volume to avoid connection timeout

--- migrations/V003__insert_data.sql
+++ migrations/V003__insert_data.sql
@@ -6,16 +6,16 @@ INSERT INTO product (id, name, picture_url, price) VALUES (4, 'Нюренбер�
 INSERT INTO product (id, name, picture_url, price) VALUES (5, 'Мюнхенская', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/2.jpg', 330.00);
 INSERT INTO product (id, name, picture_url, price) VALUES (6, 'Русская', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/1.jpg', 189.00);
 
--- Заказы (теперь со статусом и датой)
+-- Заказы (статус и дата)
 INSERT INTO orders (id, status, date_created)
 SELECT i,
        (array['pending', 'shipped', 'cancelled'])[floor(random() * 3 + 1)],
        DATE(NOW() - (random() * (NOW() + '90 days' - NOW())))
-FROM generate_series(1, 10000000) s(i);
+FROM generate_series(1, 100000) s(i);
 
 -- Состав заказов
 INSERT INTO order_product (quantity, order_id, product_id)
 SELECT floor(1 + random() * 50)::int,
        i,
        1 + floor(random() * 6)::int % 6
-FROM generate_series(1, 10000000) s(i);
+FROM generate_series(1, 100000) s(i);
